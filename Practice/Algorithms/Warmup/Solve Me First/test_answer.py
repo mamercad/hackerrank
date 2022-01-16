d2831a7f8a4bc48c7c22e74547caf7613df95fd8
Mark Mercado <mamercad@gmail.com>
2022-01-16 09:40:55 -0500

    Adding a test

---
+++ Practice/Algorithms/Warmup/Solve Me First/test_answer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pytest
+
+def solveMeFirst(a, b):
+    return a + b
+
+def test_solveMeFirst():
+    assert solveMeFirst(1, 1) == 2
+    assert solveMeFirst(1, 2) == 3
+    assert solveMeFirst(-1, 2) == 1
+    assert solveMeFirst(-1, 0) == -1
+    assert solveMeFirst(3, 7) == 10
