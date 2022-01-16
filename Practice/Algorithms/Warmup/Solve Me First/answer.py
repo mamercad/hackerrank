c151836fe337c680e19aacdc323baf9ea013111a
Mark Mercado <mamercad@gmail.com>
2022-01-16 09:33:05 -0500

    Adding a problem

---
+++ Practice/Algorithms/Warmup/Solve Me First/answer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+
+def solveMeFirst(a, b):
+    return a + b
+
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
