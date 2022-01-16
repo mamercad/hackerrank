85783a2022f9fee00a7e24df3a1e9fdb60cc974c
Mark Mercado <mamercad@gmail.com>
2022-01-16 16:07:17 -0500

    Adding another problem

---
+++ Practice/Algorithms/Warmup/Compare the Triplets/test_answer2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import pytest
+
+
+def compareTriplets(a, b):
+    # Write your code here
+    score = [0, 0]
+    c = zip(a, b)
+    for t in c:
+        if t[0] > t[1]:
+            score[0] += 1
+        elif t[0] < t[1]:
+            score[1] += 1
+    return score
+
+
+def test_compareTriplets():
+    assert compareTriplets([0, 0, 0], [0, 0, 0]) == [0, 0]
+    assert compareTriplets([1, 0, 0], [0, 0, 0]) == [1, 0]
+    assert compareTriplets([1, 1, 0], [0, 0, 0]) == [2, 0]
+    assert compareTriplets([1, 1, 1], [0, 0, 0]) == [3, 0]
+    assert compareTriplets([0, 1, 1], [1, 1, 1]) == [0, 1]
